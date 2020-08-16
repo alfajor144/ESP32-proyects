b358f3140301619d5b242f14502377b4b5caf560
Alfa-Jor 144 <54129526+alfajor144@users.noreply.github.com>
2020-08-15 21:34:35 -0300

    Update CAR_AP.ino

--- ESP32CAM/CAR_AP/CAR_AP.ino
+++ ESP32CAM/CAR_AP/CAR_AP.ino
@@ -119,9 +119,9 @@ void setup(){
 
   for (int i = 0; i < 5; i++) {
     ledcWrite(7, 10); // flash led
-    delay(200);
+    delay(50);
     ledcWrite(7, 0);
-    delay(200);
+    delay(50);
   }
 }
 
